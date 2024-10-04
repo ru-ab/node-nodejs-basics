8653bbf30cfccb5fc3694b14e8fb89ebafb398a0
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-10-04 16:28:45 +0600

    feat: implement stream write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs';
+import process from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const dirTarget = `${__dirname}/files/fileToWrite.txt`;
+
+  process.stdout.write(
+    'Please enter some data (Ctrl+C or Ctrl+D to exit):' + '\n'
+  );
+
+  const ws = fs.createWriteStream(dirTarget);
+  process.stdin.pipe(ws);
 };
 
 await write();
