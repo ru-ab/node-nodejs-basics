9fd37e852bb76a573c35e08834c065f84d901b6a
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-10-04 12:31:34 +0600

    feat: implement calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import crypto from 'node:crypto';
+import fs from 'node:fs';
+import process from 'node:process';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const sourcePath = `${__dirname}/files/fileToCalculateHashFor.txt`;
+
+  const hash = crypto.createHash('sha256');
+  const input = fs.createReadStream(sourcePath);
+
+  input
+    .pipe(hash)
+    .setEncoding('hex')
+    .on('data', (chunk) => process.stdout.write(chunk + '\n'));
 };
 
 await calculateHash();
