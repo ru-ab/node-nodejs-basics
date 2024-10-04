530885ed66894f2bb648d3a4688efae9ec90222b
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-10-04 16:45:38 +0600

    feat: implement stream transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
+import process from 'node:process';
+import { Transform, pipeline } from 'node:stream';
+
 const transform = async () => {
-    // Write your code here 
+  const reverse = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversed = [...chunk.toString()].reverse().join('');
+      callback(null, reversed + '\n');
+    },
+  });
+
+  process.stdout.write(
+    'Please enter some data (Ctrl+C or Ctrl+D to exit):' + '\n'
+  );
+
+  pipeline(process.stdin, reverse, process.stdout, (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
 };
 
 await transform();
