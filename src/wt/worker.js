61b9269a018251fc6cdd569249c1d5ec6e6efb73
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-10-04 17:37:03 +0600

    feat: implement wt

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,20 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort, workerData } from 'node:worker_threads';
+
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  try {
+    parentPort.postMessage({
+      status: 'resolved',
+      data: nthFibonacci(workerData.num),
+    });
+  } catch {
+    parentPort.postMessage({
+      status: 'error',
+      data: null,
+    });
+  }
 };
 
 sendResult();
