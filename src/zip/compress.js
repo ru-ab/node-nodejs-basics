aaf9fc88054d66cd13423b40c329b8d18e7a5d89
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-10-04 16:58:41 +0600

    feat: implement zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+import zlib from 'node:zlib';
+import stream from 'node:stream';
+import fs from 'node:fs';
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const sourcePath = `${__dirname}/files/fileToCompress.txt`;
+  const targetPath = `${__dirname}/files/archive.gz`;
+
+  const gzip = zlib.createGzip();
+  const source = fs.createReadStream(sourcePath);
+  const target = fs.createWriteStream(targetPath);
+
+  stream.pipeline(source, gzip, target, (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
 };
 
 await compress();
